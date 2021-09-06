1ed6d98d29df734ec0bff267879959bfc6745e4e
Nattaporn  Pongpluem <nattaporn.po@kbtg.tech>
2021-09-06 17:50:55 +0700

    Update Test

--- demoapp/src/main/java/com/example/demoapp/employees/EmployeeController.java
+++ demoapp/src/main/java/com/example/demoapp/employees/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.example.demoapp.employees;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,11 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeService employeeService;
+
     @GetMapping("/employees/{id}")
     public EmployeeResponse listEmployeeById(@PathVariable String id){
-        EmployeeResponse employeeResponse1 = new EmployeeResponse(1,"Name1");
-        //EmployeeResponse employeeResponse2 = new EmployeeResponse(1,"Name2");
-        return employeeResponse1;
+        //EmployeeResponse employeeResponse1 = new EmployeeResponse(1,"Name1");
+        //return employeeResponse1;
+
+        EmployeeResponse response = employeeService.getById(Integer.parseInt(id));
+        return response;
     }
 
     @GetMapping("/employees")
--- demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerWebMvcTest.java
+++ demoapp/src/test/java/com/example/demoapp/employees/EmployeeControllerWebMvcTest.java
@@ -4,11 +4,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -17,6 +18,31 @@ public class EmployeeControllerWebMvcTest {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private EmployeeService employeeService;
+
+    @Test
+    public void success_getEmployeeById() throws Exception {
+        int id = 100;
+        // Arrange
+        EmployeeResponse mockResponse = new EmployeeResponse();
+        mockResponse.setId(100);
+        mockResponse.setName("Mock name");
+        when(employeeService.getById(100)).thenReturn(mockResponse);
+        // Act
+        MvcResult result = mvc.perform(get("/employees/"+ id))
+                .andExpect(status().isOk())
+                .andReturn();
+        // Response body
+        byte[] json = result.getResponse().getContentAsByteArray();
+        ObjectMapper mapper = new ObjectMapper();
+        EmployeeResponse response = mapper.readValue(json, EmployeeResponse.class);
+        // Assert
+        assertEquals(id, response.getId());
+        assertEquals("Mock name", response.getName());
+    }
+
+    /*
     @Test
     public void getEmployeeById() throws Exception {
         int id = 1;
@@ -31,4 +57,6 @@ public class EmployeeControllerWebMvcTest {
         assertEquals(id, response.getId());
         assertEquals("Name1", response.getName());
     }
+    */
+
 }
